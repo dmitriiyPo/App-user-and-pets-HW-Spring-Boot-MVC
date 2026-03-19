e31efacc3b7c6efdba8b2de5ad98082e363680ac
Dmitriy Ponomarev <ponomarev.dmitriiy@gmail.com>
2026-03-19 21:42:09 +0600

    add README.md

---
+++ src/main/java/dev/sorokin/userandpets/UserAndPetsApplication.java
@@ -0,0 +1,13 @@
+package dev.sorokin.userandpets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAndPetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserAndPetsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/sorokin/userandpets/UserAndPetsApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.sorokin.userandpets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAndPetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
