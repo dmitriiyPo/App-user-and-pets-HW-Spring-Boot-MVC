16183c63156fbb6a264f2ab44a5e93cbe0eebe89
Dmitriy Ponomarev <ponomarev.dmitriiy@gmail.com>
2026-03-19 21:45:11 +0600

    [*] README.md

--- src/main/java/dev/sorokin/userandpets/UserAndPetsApplication.java
+++ src/main/java/dev/sorokin/userandpets/UserAndPetsApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class UserAndPetsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UserAndPetsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(UserAndPetsApplication.class, args);
+    }
 
 }
